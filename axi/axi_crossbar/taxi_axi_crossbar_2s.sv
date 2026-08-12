0d6f659810d37f447dcb12c84f556b9e62239675
ssssss-dot <shenjiexiangs@gmail.com>
2026-08-12 19:56:45 +0800

    测试

---
+++ axi/axi_crossbar/taxi_axi_crossbar_2s.sv
@@ -0,0 +1,161 @@
+// SPDX-License-Identifier: CERN-OHL-S-2.0
+/*
+
+Copyright (c) 2018-2025 FPGA Ninja, LLC
+
+Authors:
+- Alex Forencich
+
+*/
+
+`resetall
+`timescale 1ns / 1ps
+`default_nettype none
+
+/*
+ * AXI4 crossbar
+ */
+module taxi_axi_crossbar_2s #
+(
+    // Number of AXI outputs (master interfaces)
+    parameter M_COUNT = 2,
+    // Address width in bits for address decoding
+    parameter ADDR_W = 32,
+    // TODO fix parametrization once verilator issue 5890 is fixed
+    // Number of concurrent unique IDs
+    parameter S_THREADS = {4{32'd2}},
+    // Number of concurrent operations for each slave interface
+    // 1 concatenated fields of 32 bits
+    parameter S_ACCEPT = {4{32'd16}},
+    // Number of regions per master interface
+    parameter M_REGIONS = 1,
+    // Master interface base addresses
+    // M_COUNT concatenated fields of M_REGIONS concatenated fields of ADDR_W bits
+    // set to zero for default addressing based on M_ADDR_W
+    parameter M_BASE_ADDR = {
+    32'h4000_0000, // m_axi[1]
+    32'h0000_0000  // m_axi[0]
+    },
+    // Master interface address widths
+    // M_COUNT concatenated fields of M_REGIONS concatenated fields of 32 bits
+    parameter M_ADDR_W = {
+    32'd12,  // m_axi[1]：UART，0x4000_0000～0x4000_0FFF
+    32'd30   // m_axi[0]：DDR， 0x0000_0000～0x3FFF_FFFF
+    },
+    // Number of concurrent operations for each master interface
+    // M_COUNT concatenated fields of 32 bits
+    parameter M_ISSUE = {M_COUNT{32'd4}},
+    // Secure master (fail operations based on awprot/arprot)
+    // M_COUNT bits
+    parameter M_SECURE = {M_COUNT{1'b0}},
+    // Slave interface AW channel register type (input)
+    // 0 to bypass, 1 for simple buffer, 2 for skid buffer
+    parameter S_AW_REG_TYPE = {4{2'd0}},
+    // Slave interface W channel register type (input)
+    // 0 to bypass, 1 for simple buffer, 2 for skid buffer
+    parameter S_W_REG_TYPE = {4{2'd0}},
+    // Slave interface B channel register type (output)
+    // 0 to bypass, 1 for simple buffer, 2 for skid buffer
+    parameter S_B_REG_TYPE = {4{2'd1}},
+    // Slave interface AR channel register type (input)
+    // 0 to bypass, 1 for simple buffer, 2 for skid buffer
+    parameter S_AR_REG_TYPE = {4{2'd0}},
+    // Slave interface R channel register type (output)
+    // 0 to bypass, 1 for simple buffer, 2 for skid buffer
+    parameter S_R_REG_TYPE = {4{2'd2}},
+    // Master interface AW channel register type (output)
+    // 0 to bypass, 1 for simple buffer, 2 for skid buffer
+    parameter M_AW_REG_TYPE = {M_COUNT{2'd1}},
+    // Master interface W channel register type (output)
+    // 0 to bypass, 1 for simple buffer, 2 for skid buffer
+    parameter M_W_REG_TYPE = {M_COUNT{2'd2}},
+    // Master interface B channel register type (input)
+    // 0 to bypass, 1 for simple buffer, 2 for skid buffer
+    parameter M_B_REG_TYPE = {M_COUNT{2'd0}},
+    // Master interface AR channel register type (output)
+    // 0 to bypass, 1 for simple buffer, 2 for skid buffer
+    parameter M_AR_REG_TYPE = {M_COUNT{2'd1}},
+    // Master interface R channel register type (input)
+    // 0 to bypass, 1 for simple buffer, 2 for skid buffer
+    parameter M_R_REG_TYPE = {M_COUNT{2'd0}}
+)
+(
+    input  wire logic   clk,
+    input  wire logic   rst,
+
+    /*
+     * AXI4 slave interface
+     */
+    taxi_axi_if.wr_slv  s_axi_wr[4],
+    taxi_axi_if.rd_slv  s_axi_rd[4],
+
+    /*
+     * AXI4 master interfaces
+     */
+    taxi_axi_if.wr_mst  m_axi_wr[2],
+    taxi_axi_if.rd_mst  m_axi_rd[2]
+);
+
+taxi_axi_crossbar_wr #(
+    .S_COUNT(4),
+    .M_COUNT(2),
+    .ADDR_W(ADDR_W),
+    .S_THREADS(S_THREADS),
+    .S_ACCEPT(S_ACCEPT),
+    .M_REGIONS(M_REGIONS),
+    .M_BASE_ADDR(M_BASE_ADDR),
+    .M_ADDR_W(M_ADDR_W),
+    .M_ISSUE(M_ISSUE),
+    .M_SECURE(M_SECURE),
+    .S_AW_REG_TYPE(S_AW_REG_TYPE),
+    .S_W_REG_TYPE(S_W_REG_TYPE),
+    .S_B_REG_TYPE(S_B_REG_TYPE)
+)
+wr_inst (
+    .clk(clk),
+    .rst(rst),
+
+    /*
+     * AXI slave interface
+     */
+    .s_axi_wr(s_axi_wr),
+
+    /*
+     * AXI master interfaces
+     */
+    .m_axi_wr(m_axi_wr)
+);
+
+taxi_axi_crossbar_rd #(
+    .S_COUNT(4),
+    .M_COUNT(2),
+    .ADDR_W(ADDR_W),
+    .S_THREADS(S_THREADS),
+    .S_ACCEPT(S_ACCEPT),
+    .M_REGIONS(M_REGIONS),
+    .M_BASE_ADDR(M_BASE_ADDR),
+    .M_ADDR_W(M_ADDR_W),
+    .M_ISSUE(M_ISSUE),
+    .M_SECURE(M_SECURE),
+    .S_AR_REG_TYPE(S_AR_REG_TYPE),
+    .S_R_REG_TYPE(S_R_REG_TYPE)
+)
+rd_inst (
+    .clk(clk),
+    .rst(rst),
+
+    /*
+     * AXI slave interface
+     */
+    .s_axi_rd(s_axi_rd),
+
+    /*
+     * AXI master interfaces
+     */
+    .m_axi_rd(m_axi_rd)
+);
+
+
+endmodule
+
+`resetall
