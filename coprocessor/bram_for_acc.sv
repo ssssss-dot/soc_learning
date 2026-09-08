a915c48c6d69b6610a042e4960b0fadd98c31c17
ssssss-dot <shenjiexiangs@gmail.com>
2026-09-09 00:57:01 +0800

    fix: infer accelerator buffer as block RAM

--- coprocessor/bram_for_acc.sv
+++ coprocessor/bram_for_acc.sv
@@ -29,14 +29,14 @@ module bram_for_acc(
 );
 
 (*ram_style = "block" *) reg [`DataBus] bram [`RAM_SIZE];
-reg [14:0] ptr_wr;
+reg [13:0] ptr_wr;
 reg [15:0] rd_count;// 当前输出拍编号，同时作为BRAM读地址
 reg [15:0] rd_len_reg;
 wire [15:0] total_beats;
 wire ram2ddr_start;// 写描述符握手产生的单周期启动信号
 
 assign ram2ddr_start = ctrl_wr_desc_valid && ctrl_wr_desc_ready;
-assign total_beats = (rd_len_reg + 16'd3) >> 2;//dma一拍搬运一个字，len的单位是字节，做转换，判断last什么时候拉高
+assign total_beats = ({1'b0, rd_len_reg} + 17'd3) >> 2;//dma一拍搬运一个字，len的单位是字节，除以4做转换，判断last什么时候拉高，同时扩展一位防止溢出
 assign last_o = valid_o && (rd_count == total_beats - 16'd1);
 
 assign keep_o =
@@ -56,12 +56,23 @@ end
 
 assign ready_i = !write_blocked;
 
+//bram不能放在有复位的always块中
 //dma从ddr搬运数据到ram
 always @(posedge clk or negedge rst_n)begin
     if(!rst_n)begin
         ptr_wr <= 'd0;
     end
     else if(valid_i && ready_i)begin
+        if(last_i)begin
+            ptr_wr <= 'd0;
+        end
+        else begin
+            ptr_wr <= ptr_wr +1'b1;
+        end
+    end
+end
+always @(posedge clk) begin
+    if(valid_i && ready_i)begin
         if(keep_i[0])begin
             bram[ptr_wr][7:0] <= data_i[7:0];
         end
@@ -73,13 +84,7 @@ always @(posedge clk or negedge rst_n)begin
         end
         if(keep_i[3])begin
             bram[ptr_wr][31:24] <= data_i[31:24];
-        end
-        if(last_i)begin
-            ptr_wr <= 'd0;
-        end
-        else begin
-            ptr_wr <= ptr_wr +1'b1;
-        end
+    end
     end
 end
 
@@ -87,13 +92,11 @@ end
 always @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
         rd_count <= '0;
-        data_o   <= '0;
         valid_o  <= 1'b0;
     end
     else if (ram2ddr_start) begin
         //提前准备第一拍,防止握手时同时更新data_o导致采样出问题
         rd_count <= '0;
-        data_o   <= bram[0];
         valid_o  <= (ram2ddr_len != 16'd0);
     end
     else if (valid_o && ready_o) begin
@@ -105,8 +108,17 @@ always @(posedge clk or negedge rst_n) begin
         else begin
             //当前拍已经传完，准备下一拍，keep_o会告诉dma，由dma筛选有效字节
             rd_count <= rd_count + 16'd1;
-            data_o   <= bram[rd_count + 16'd1];
         end
     end
 end
+
+always @(posedge clk) begin
+    if (ram2ddr_start) begin
+        data_o <= bram[14'd0];
+    end
+    else if (valid_o && ready_o && !last_o) begin
+        data_o <= bram[rd_count[13:0] + 14'd1];
+    end
+end
+
 endmodule
